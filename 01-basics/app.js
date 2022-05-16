792055a7fa579de20866336e6997b8cf6dcee3e6
matteotagliatti <matteotagliatti@gmail.com>
2022-05-16 18:25:21 +0200

    http module

--- 01-basics/app.js
+++ 01-basics/app.js
@@ -105,3 +105,20 @@ readFile("./08/first.txt", "utf8", (err, res) => {
     );
   });
 });
+
+// 10. Built-in Modules: HTTP
+// Setup a web server (Basics)
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  // req = request, res = response
+  if (req.url === "/") {
+    /* res.write('Hello World') */ // Skippable passage because we can put our message directly in the res.end()
+    res.end("Hello World");
+  }
+  if (req.url === "/about") {
+    res.end("About page");
+  }
+});
+server.listen(5000); // Start the server on port 5000

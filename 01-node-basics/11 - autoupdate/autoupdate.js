6f134b831ac387fce6c57c554f52518d6bda8f20
matteotagliatti <matteotagliatti@gmail.com>
2022-07-07 11:46:47 +0200

    rm node_modules

--- 01-node-basics/11 - autoupdate/autoupdate.js
+++ 01-node-basics/11 - autoupdate/autoupdate.js
@@ -1,7 +1,7 @@
 /* 
 Autoupdate
     - Install as a dev dependency with `npm install --save-dev nodemon`. Useful for autoload the application at every changes.
-    - In package.json we have specify a `start` script that run and reload at every changes our file `11/autoload.js`. We have done this preaviously installing a dev 
+    - In package.json we have specify a `start` script that run and reload at every changes our file `autoload.js`. We have done this preaviously installing a dev 
  */
 
 console.log("try to run the app");
--- 01-node-basics/14 - events/app.js
+++ 01-node-basics/14 - events/app.js
@@ -1,2 +1,4 @@
 // Event Driver Programming. User Heavily in Node.js
+// We listen to specific event and when that event is triggered, we execute a function.
 const EventEmitter = require("events");
+console.log("ciao");
